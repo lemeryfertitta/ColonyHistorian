0ca8f87b8825a200dcf9612129a016ce1c0d5375
Luke Emery-Fertitta <lemeryfertitta@gmail.com>
2023-10-31 08:53:24 -0700

    Only capture frames on game log updates

--- src/historian.js
+++ src/historian.js
@@ -1,11 +1,12 @@
 const mediaSource = new MediaSource();
 mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
 let mediaRecorder;
+let observer;
 let recordedBlobs;
 let sourceBuffer;
 let stream;
 
-const rightSide = document.getElementById('in_game_ab_right');
+const rightSide = document.getElementById("in_game_ab_right");
 const recordButton = document.createElement("button");
 recordButton.textContent = "Start Recording";
 rightSide.insertBefore(recordButton, rightSide.firstChild);
@@ -31,7 +32,6 @@ function handleDataAvailable(event) {
 
 function handleStop(event) {
     console.log('Recorder stopped: ', event);
-    const superBuffer = new Blob(recordedBlobs, { type: 'video/webm' });
 }
 
 function toggleRecording() {
@@ -48,7 +48,7 @@ function startRecording() {
     let options = { mimeType: 'video/webm' };
     recordedBlobs = [];
     const canvas = document.querySelector('canvas');
-    stream = canvas.captureStream(); // frames per second
+    stream = canvas.captureStream(0);
     console.log('Started stream capture from canvas element: ', stream);
     mediaRecorder = new MediaRecorder(stream, options);
     console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
@@ -56,12 +56,30 @@ function startRecording() {
     downloadButton.disabled = true;
     mediaRecorder.onstop = handleStop;
     mediaRecorder.ondataavailable = handleDataAvailable;
-    mediaRecorder.start(100); // collect 100ms of data
+    mediaRecorder.start();
     console.log('MediaRecorder started', mediaRecorder);
+
+    const gameLog = document.getElementById("game-log-text");
+    // Options for the observer (which mutations to observe)
+    const config = { attributes: false, childList: true, subtree: true };
+
+    // Callback function to execute when mutations are observed
+    const callback = (mutationList, _) => {
+        console.log("Game log changed: ", mutationList);
+        // TODO: Look only for game events which cause a change in the board state.
+        stream.getVideoTracks()[0].requestFrame();
+    };
+
+    // Create an observer instance linked to the callback function
+    observer = new MutationObserver(callback);
+
+    // Start observing the target node for configured mutations
+    observer.observe(gameLog, config);
 }
 
 function stopRecording() {
     mediaRecorder.stop();
+    observer.disconnect();
     console.log('Recorded Blobs: ', recordedBlobs);
 }
 
